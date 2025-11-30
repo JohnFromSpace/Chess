40e74e408a5a31ff8a828287b7b612a5231bb451
JohnFromSpace <96446768+JohnFromSpace@users.noreply.github.com>
2025-11-30 22:10:41 +0200

    Updated Message.

--- common/src/main/java/com/example/chess/common/Message.java
+++ common/src/main/java/com/example/chess/common/Message.java
@@ -1,15 +1,66 @@
 package com.example.chess.common;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class Message {
     public String type;
     public String corrId;
-    public JsonObject payload;
+    public JsonObject data = new JsonObject();
 
-    public Message(String type, String corrId, JsonObject payload) {
+    public Message(String type, String corrId, JsonObject data) {
         this.type = type;
         this.corrId = corrId;
-        this.payload = payload;
+        this.data = data;
+    }
+
+    public Message(String type, String corrId) {
+        this.type = type;
+        this.corrId = corrId;
+    }
+
+    public static Message of(String type, String corrId) {
+        return new Message(type, corrId);
+    }
+
+    public Message put(String key, String value) {
+        data.addProperty(key, value);
+        return this;
+    }
+
+    public Message put(String key, int value) {
+        data.addProperty(key, value);
+        return this;
+    }
+
+    public Message put(String key, boolean value) {
+        data.addProperty(key, value);
+        return this;
+    }
+
+    public Message put(String key, JsonElement value) {
+        data.add(key, value);
+        return this;
+    }
+
+    public String getRequiredString(String field) {
+        if (!data.has(field) || data.get(field).isJsonNull()) {
+            throw new IllegalArgumentException("Missing field: " + field);
+        }
+        return data.get(field).getAsString();
+    }
+
+    public int getIntOrDefault(String field, int def) {
+        if (!data.has(field) || data.get(field).isJsonNull()) {
+            return def;
+        }
+        return data.get(field).getAsInt();
+    }
+
+    public boolean getBooleanOrDefault(String field, boolean def) {
+        if (!data.has(field) || data.get(field).isJsonNull()) {
+            return def;
+        }
+        return data.get(field).getAsBoolean();
     }
 }
