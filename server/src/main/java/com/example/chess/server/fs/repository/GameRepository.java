586103f261245e94fba1acc39b652ea25ac0cc02
JohnFromSpace <96446768+JohnFromSpace@users.noreply.github.com>
2025-11-30 20:44:45 +0200

    Fixes.

--- server/src/main/java/com/example/chess/server/fs/repository/GameRepository.java
+++ server/src/main/java/com/example/chess/server/fs/repository/GameRepository.java
@@ -5,12 +5,7 @@ import java.util.Optional;
 import com.example.chess.common.GameModels.Game;
 
 public interface GameRepository {
-    List<Game> findAllGames();
-
     Optional<Game> findGameById(String id);
     void saveGame(Game game);
-
-    Optional<Game> findById(String id);
-
     List<Game> findGamesForUser(String username);
 }
