bf356a3ea7890932faef3980fe595866790b36b4
JohnFromSpace <96446768+JohnFromSpace@users.noreply.github.com>
2025-12-23 03:42:23 +0200

    Fixed.

--- common/src/main/java/com/example/chess/common/pieces/Rook.java
+++ common/src/main/java/com/example/chess/common/pieces/Rook.java
@@ -8,15 +8,13 @@ public final class Rook extends Piece {
     public Rook(Color color) { super(color); }
 
     @Override
-    public char toChar() {
-        return getColor() == Color.WHITE ? 'R' : 'r';
-    }
+    public char toChar() { return getColor() == Color.WHITE ? 'R' : 'r'; }
 
     @Override
     public boolean canMove(Board board, Move m) {
-        int dx = Math.abs(m.toCol - m.fromCol);
-        int dy = Math.abs(m.toRow - m.fromRow);
-        if (!(dx == 0 || dy == 0)) return false;
+        int dr = m.toRow - m.fromRow;
+        int dc = m.toCol - m.fromCol;
+        if (!((dr == 0) ^ (dc == 0))) return false;
         return isPathClear(board, m.fromRow, m.fromCol, m.toRow, m.toCol);
     }
 }
