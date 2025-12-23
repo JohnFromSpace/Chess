7f7e1c41c6dae2a61cb050013599053ce1ab16cf
JohnFromSpace <96446768+JohnFromSpace@users.noreply.github.com>
2025-12-23 03:41:39 +0200

    Fixed.

--- common/src/main/java/com/example/chess/common/pieces/Bishop.java
+++ common/src/main/java/com/example/chess/common/pieces/Bishop.java
@@ -8,15 +8,13 @@ public final class Bishop extends Piece {
     public Bishop(Color color) { super(color); }
 
     @Override
-    public char toChar() {
-        return getColor() == Color.WHITE ? 'B' : 'b';
-    }
+    public char toChar() { return getColor() == Color.WHITE ? 'B' : 'b'; }
 
     @Override
     public boolean canMove(Board board, Move m) {
         int dx = Math.abs(m.toCol - m.fromCol);
         int dy = Math.abs(m.toRow - m.fromRow);
-        if (dx != dy) return false;
+        if (dx == 0 || dx != dy) return false;
         return isPathClear(board, m.fromRow, m.fromCol, m.toRow, m.toCol);
     }
 }
